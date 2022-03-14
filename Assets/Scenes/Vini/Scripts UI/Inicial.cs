87b1e801a34260de7283770d46737ff16829e637
vinitfrek <vinitfrek12@gmail.com>
2022-03-14 19:34:53 -0300

    musica e pasta adicionada
    ajustesx

--- Assets/Scenes/Vini/Scripts UI/Inicial.cs
+++ Assets/Scenes/Vini/Scripts UI/Inicial.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Inicial : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //anexar ao Audio Source
+    [SerializeField]AudioSource m_AudioSource;
+    float valueSlider;
+    bool canIncrease;
+
+
     void Start()
     {
-        
+        //para poder começar a aumentar a musica
+        canIncrease = true;
+        valueSlider = 0f;    
     }
 
     // Update is called once per frame
     void Update()
     {
+        //mudar o valor e periodicamente atualiza-lo
+        m_AudioSource.volume = valueSlider;
+        //condição para aumentar o volume
+        if (valueSlider < 0.25f && canIncrease)
+        {
+            StartCoroutine(AumentandoVolume());
+        }
         
+    }
+    //coroutine para aumentar o volume suavemente
+    IEnumerator AumentandoVolume()
+    {
+        canIncrease = false;
+        yield return new WaitForSeconds(0.3f); 
+        valueSlider += 0.02f;
+        yield return new WaitForSeconds(0.3f);
+        canIncrease = true;
     }
 }
