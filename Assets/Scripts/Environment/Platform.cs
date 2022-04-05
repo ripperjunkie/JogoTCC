8383892eaab89fb99fe48589d169fb6fbb35a749
guilhermezigs123 <gui_2009ar@hotmail.com>
2022-04-05 14:45:18 -0300

    plataforma

---
+++ Assets/Scripts/Environment/Platform.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+public GameObject plataform = null;
+public GameObject player = null;
+public  Transform EndPoint;
+private Vector3 initialPoint;
+  public bool active = false;
+   public bool begin = false;
+   public float speedPlataform = 0;
+   void OnTriggerEnter(Collider col)
+  {
+      if(col.gameObject == player)
+      {
+         active= true;
+         player.transform.parent = transform;        
+      }   
+  }
+  void OnTriggerExit(Collider col)
+  {
+    if(col.gameObject == player)
+    {
+       active= false;  
+        player.transform.parent = null;   
+    }
+
+  }
+    public IEnumerator ActiveLever()
+  {
+      if(begin)
+      {
+        while(Vector3.Distance(plataform.transform.position, EndPoint.transform.position) > 0)
+          {
+            plataform.transform.position =Vector3.Lerp(plataform.transform.position,EndPoint.transform.position, speedPlataform* Time.deltaTime);         
+              yield return new WaitForSeconds(0.02f);
+          }
+      }
+   
+      else 
+      {
+          while(Vector3.Distance(plataform.transform.position, initialPoint) > 0)
+        {
+          plataform.transform.position = Vector3.Lerp(plataform.transform.position, initialPoint, speedPlataform * Time.deltaTime);       
+           yield return new WaitForSeconds(0.02f);
+        }
+      }
+  }
+ public void ChangeRoute()
+  {
+    if(begin)
+    {
+      begin = false;
+    }
+    else
+    {
+      begin = true;
+    }
+  }
+}
--- Assets/Scripts/Environment/PushLever.cs
+++ Assets/Scripts/Environment/PushLever.cs
@@ -4,73 +4,46 @@ using UnityEngine;
 
 public class PushLever : MonoBehaviour
 {
-  public GameObject plataform = null;
-  public GameObject player = null;
-  public  Transform EndPoint;
-  private Vector3 initialPoint;
+ 
   public float speedPlataform = 0;
   public bool active = false;
-  public bool begin = false;
+  
+   public  Platform platform;
 
       void Start()
     {
-        initialPoint= plataform.transform.position;
+    
     }
-   void OnTriggerEnter(Collider col)
+       void OnTriggerEnter(Collider col)
   {
-      if(col.gameObject == player)
+      if(col.gameObject.CompareTag("Player"))
       {
          active= true;
-         player.transform.parent = transform;        
       }   
   }
-  void OnTriggerExit(Collider col)
+         void OnTriggerExit(Collider col)
   {
-    if(col.gameObject == player)
-    {
-       active= false;  
-        player.transform.parent = null;   
-    }
-
+      if(col.gameObject.CompareTag("Player"))
+      {
+         active= false;
+      }   
   }
+
   void Update()
   {
     if(Input.GetKeyDown(KeyCode.E) && active)
       {
-         ChangeRoute();
-         StartCoroutine(ActiveLever());
+        
+       Move();
       }
+  }
+  void Move()
+  {
+    if(active)
+    {
+      platform.StartCoroutine(platform.ActiveLever());
+    }
+    
   }
 
-    IEnumerator ActiveLever()
-  {
-      if(begin)
-      {
-        while(Vector3.Distance(plataform.transform.position, EndPoint.transform.position) > 0)
-          {
-            plataform.transform.position =Vector3.Lerp(plataform.transform.position,EndPoint.transform.position, speedPlataform* Time.deltaTime);         
-              yield return new WaitForSeconds(0.02f);
-          }
-      }
-   
-      else 
-      {
-          while(Vector3.Distance(plataform.transform.position, initialPoint) > 0)
-        {
-          plataform.transform.position = Vector3.Lerp(plataform.transform.position, initialPoint, speedPlataform * Time.deltaTime);       
-           yield return new WaitForSeconds(0.02f);
-        }
-      }
-  }
-  void ChangeRoute()
-  {
-    if(begin)
-    {
-      begin = false;
-    }
-    else
-    {
-      begin = true;
-    }
-  }
 }
