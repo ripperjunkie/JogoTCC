1a0278b5bb4057d1e2c2c8bfdeed6910c321d55a
vinitfrek <vinitfrek12@gmail.com>
2022-04-04 21:19:53 -0300

    adicionado o son passos e ajustado os sons
    falta ajustar o fade errei a logica mas deixei sem fazer para ter pelo menos troca da musica, no script

--- Assets/Scripts/Sound SC/RegionChangeSound.cs
+++ Assets/Scripts/Sound SC/RegionChangeSound.cs
@@ -7,13 +7,18 @@ public class RegionChangeSound : MonoBehaviour
     public AudioSource Song1;
     public AudioSource Song2;
 
-    bool noInfinite;
-    bool noInfinite2;
+    private bool noInfinite;
+    private bool noInfinite2;
+    private bool canIncrease;
+    private float sliderVol;
+    private float sliderVol2;
+    
     // Start is called before the first frame update
     void Start()
     {
         noInfinite = false;
         noInfinite2 = false;
+        canIncrease = true;
     }
 
     // Update is called once per frame
@@ -26,33 +31,43 @@ public class RegionChangeSound : MonoBehaviour
         
         if (other.gameObject.CompareTag("SoundRange1"))
         {
-            noInfinite2 = true;
-            if (noInfinite)
-            {
-                Song1.enabled = true;
-                Song2.enabled = false;
-            }
-            if (noInfinite)
-            {
-                noInfinite = !noInfinite;
-            }
+            SongChange(Song1, Song2, noInfinite, noInfinite2);
         }
         
         if (other.gameObject.CompareTag("SoundRange2"))
         {
-            noInfinite = true;
-            if (noInfinite2)
-            {
-                Song1.enabled = false;
-                Song2.enabled = true;
-                Song2.Play();
-            }
-            if (noInfinite2)
-            {
-                noInfinite2 = !noInfinite2;
-            }
+            SongChange(Song2, Song1, noInfinite2, noInfinite);
         }
     }
 
+    private void SongChange(AudioSource _audio1, AudioSource _audio2, bool _condition1, bool _condition2)
+    {
+       _condition2 = true;
+        if (_condition1)
+        {
+            _audio1.enabled = true;
+            _audio2.enabled = false;
+            _audio2.Stop();
+        }
+        if (_condition1)
+        {
+            _condition1 = !_condition1;
+        }
+        if (_audio1.isPlaying == false) 
+        { 
+            _audio1.Play(); 
+        }
+        if (_audio2.isPlaying) _audio2.Stop();
+
+    }
+    private IEnumerator EVolumeUp()
+    {
+
+        canIncrease = false;
+        yield return new WaitForSeconds(0.3f);
+        sliderVol += 0.015f;
+        yield return new WaitForSeconds(0.3f);
+        canIncrease = true;
+    }// ainda não terminei 
 
 }
