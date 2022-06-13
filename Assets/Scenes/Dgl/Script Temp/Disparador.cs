224f0d4b0212cacad3e9543de51926a6add6ec88
DGl182 <douglaspescuma@gmail.com>
2022-06-13 15:44:54 -0300

    disparador
    dispara

--- Assets/Scenes/Dgl/Script Temp/Disparador.cs
+++ Assets/Scenes/Dgl/Script Temp/Disparador.cs
@@ -5,13 +5,25 @@ using UnityEngine;
 public class Disparador : MonoBehaviour
 {
     public ScreamShot screamShot;
+    public GameObject canvasImage;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             screamShot.TakeShot();
+            if (canvasImage)
+            {
+                canvasImage.SetActive(true);
+            }
             Destroy(this);
         }
+    }
+
+    IEnumerator OpenAndClose()
+    {
+        GetComponent<Collider>().enabled = false;
+        canvasImage.transform.LeanMove(Vector3.zero, 1.5f).setEaseInOutQuart();//setOnComplete(CanvasOff);
+        yield return new WaitForSeconds(1f);
     }
 }
